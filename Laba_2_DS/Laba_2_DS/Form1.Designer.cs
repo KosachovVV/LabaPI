e9c6d1603706440dfb51d8a397f0f137a20319e3
KosachovVV <incarnation_of_reality@mail.ru>
2012-11-04 20:12:56 +0200

    first commit

---
+++ Laba_2_DS/Laba_2_DS/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Laba_2_DS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BInpfromfile = new System.Windows.Forms.Button();
+            this.BExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BCalc = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TBTplus = new System.Windows.Forms.TextBox();
+            this.TBTminus = new System.Windows.Forms.TextBox();
+            this.TBG = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Busestdvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BInpfromfile
+            // 
+            this.BInpfromfile.Location = new System.Drawing.Point(12, 387);
+            this.BInpfromfile.Name = "BInpfromfile";
+            this.BInpfromfile.Size = new System.Drawing.Size(75, 38);
+            this.BInpfromfile.TabIndex = 0;
+            this.BInpfromfile.Text = "Ввод из  файла";
+            this.BInpfromfile.UseVisualStyleBackColor = true;
+            this.BInpfromfile.Visible = false;
+            // 
+            // BExit
+            // 
+            this.BExit.Location = new System.Drawing.Point(518, 395);
+            this.BExit.Name = "BExit";
+            this.BExit.Size = new System.Drawing.Size(75, 23);
+            this.BExit.TabIndex = 1;
+            this.BExit.Text = "Выход";
+            this.BExit.UseVisualStyleBackColor = true;
+            this.BExit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BCalc
+            // 
+            this.BCalc.Location = new System.Drawing.Point(265, 395);
+            this.BCalc.Name = "BCalc";
+            this.BCalc.Size = new System.Drawing.Size(75, 23);
+            this.BCalc.TabIndex = 2;
+            this.BCalc.Text = "Вычислить";
+            this.BCalc.UseVisualStyleBackColor = true;
+            this.BCalc.Visible = false;
+            this.BCalc.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 187);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // TBTplus
+            // 
+            this.TBTplus.Location = new System.Drawing.Point(12, 205);
+            this.TBTplus.Multiline = true;
+            this.TBTplus.Name = "TBTplus";
+            this.TBTplus.ReadOnly = true;
+            this.TBTplus.Size = new System.Drawing.Size(165, 152);
+            this.TBTplus.TabIndex = 5;
+            // 
+            // TBTminus
+            // 
+            this.TBTminus.Location = new System.Drawing.Point(221, 205);
+            this.TBTminus.Multiline = true;
+            this.TBTminus.Name = "TBTminus";
+            this.TBTminus.ReadOnly = true;
+            this.TBTminus.Size = new System.Drawing.Size(167, 152);
+            this.TBTminus.TabIndex = 6;
+            // 
+            // TBG
+            // 
+            this.TBG.Location = new System.Drawing.Point(426, 205);
+            this.TBG.Multiline = true;
+            this.TBG.Name = "TBG";
+            this.TBG.ReadOnly = true;
+            this.TBG.Size = new System.Drawing.Size(167, 152);
+            this.TBG.TabIndex = 7;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(353, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 187);
+            this.dataGridView2.TabIndex = 8;
+            this.dataGridView2.Visible = false;
+            // 
+            // Busestdvar
+            // 
+            this.Busestdvar.Location = new System.Drawing.Point(93, 388);
+            this.Busestdvar.Name = "Busestdvar";
+            this.Busestdvar.Size = new System.Drawing.Size(75, 37);
+            this.Busestdvar.TabIndex = 9;
+            this.Busestdvar.Text = "В соотв. с вариантом";
+            this.Busestdvar.UseVisualStyleBackColor = true;
+            this.Busestdvar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 457);
+            this.Controls.Add(this.Busestdvar);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.TBG);
+            this.Controls.Add(this.TBTminus);
+            this.Controls.Add(this.TBTplus);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BCalc);
+            this.Controls.Add(this.BExit);
+            this.Controls.Add(this.BInpfromfile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DS_Laba_2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BInpfromfile;
+        private System.Windows.Forms.Button BExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BCalc;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TBTplus;
+        private System.Windows.Forms.TextBox TBTminus;
+        private System.Windows.Forms.TextBox TBG;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button Busestdvar;
+    }
+}
+
---
+++ Laba_2_DS/Laba_2_DS/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Laba_2_DS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        int[][] Matrix = new int[0][];
+        bool inpfromfile;
+
+        private void stdmatr(ref int[][] a)
+        {
+            int[][] b = new int[7][];
+            a[0] = new int[7] { 0, 1, 0, 0, 0, 0, 0 };
+            a[1] = new int[7] { 0, 0, 1, 0, 0, 0, 0 };
+            a[2] = new int[7] { 1, 0, 0, 0, 1, 0, 0 };
+            a[3] = new int[7] { 0, 0, 1, 1, 0, 0, 0 };
+            a[4] = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            a[5] = new int[7] { 0, 0, 0, 0, 1, 0, 1 };
+            a[6] = new int[7] { 0, 0, 0, 0, 0, 0, 1 }; 
+        }
+
+        static void Output_Matrix(int[][] Mas, DataGridView dtgrdvw)
+        {
+            int col = Mas[0].Length;
+            dtgrdvw.ColumnCount = 0;
+            dtgrdvw.ColumnCount = col;
+            dtgrdvw.Rows.Add(Mas.Length);
+            for (int i = 0; i < col; i++)
+            {
+                dtgrdvw.Columns[i].ReadOnly = true;
+                dtgrdvw.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                dtgrdvw.Columns[i].Name = ("x"+(i+1)).ToString();
+                dtgrdvw.Columns[i].Width = 25;
+            }
+            for (int i = 0; i < Mas.Length; i++)
+                for (int j = 0; j < Mas[i].Length; j++)
+                    dtgrdvw.Rows[i].Cells[j].Value = Mas[i][j].ToString();
+        }
+
+        private void Tplus(ref int[] vec, int stind, int k)
+        {           
+            int i = stind;
+            for (int l = stind, j = 0; j < Matrix[l].Length; j++)                
+                        if (Matrix[l][j] == 1)
+                        {
+                            if ((vec[j] != 10 && j != i))
+                            {
+                                stind = j;
+                                vec[stind] = k;
+                                Tplus(ref vec, stind, ++k);
+                            }
+                        }
+        }
+
+        private void Tminus(ref int[] vec, int stind, int k)
+        {
+            int i = stind;
+            for (int l = stind, j = 0; j < Matrix.Length; j++)
+                if (Matrix[j][l] == 1)
+                {
+                    if ((vec[j] != 10 && j != i))
+                    {
+                        stind = j;
+                        vec[stind] = k;
+                        Tminus(ref vec, stind, ++k);
+                    }
+                }
+        }
+
+        private void newstartindex(int[] g,ref int i)
+        {
+            try
+            {
+                {
+                    for (int p = 0; p < g.Length - 1; p++)
+                        if ((g[p] != 0 && g[p + 1] == 0))
+                        {
+                            i = p + 1;
+                            //throw new Exception("Новый начальный элемент = " + (i + 1));
+                            throw new Exception();
+                        }
+                        else i = g.Length;
+                }
+            }
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            catch (Exception)
+            { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {                
+            Array.Resize(ref Matrix, 7);
+            stdmatr(ref Matrix);
+            int i = 2;
+            int count = 1;
+            Output_Matrix(Matrix, dataGridView1);
+            int[] Tp = {0,0,0,0,0,0,0};
+            int[] Tm = new int[Matrix.Length];
+            int[] G = new int[Matrix.Length];
+            while (i < Matrix.Length) 
+            {
+                Tp[i] = 10;
+                Tm[i] = 10;
+                Tplus(ref Tp, i, 1);
+                Tminus(ref Tm, i, 1);
+                for (int k = 0; k < Tp.Length; k++)
+                    if (Tp[k] != 0 && Tm[k] != 0)
+                        G[k] = k + 1;
+                TBTplus.Text += "Tplus" + count + "={  ";
+                for (int k = 0; k < Tp.Length; k++)
+                    if (Tp[k] != 0)
+                        TBTplus.Text += "x" + Convert.ToString(k + 1) + "; ";
+                TBTplus.Text += "}" + Environment.NewLine;
+                TBTminus.Text += "Tminus" + count + "={  ";
+                for (int k = 0; k < Tm.Length; k++)
+                    if (Tm[k] != 0)
+                        TBTminus.Text += "x" + Convert.ToString(k + 1) + "; ";
+                TBTminus.Text += "}" + Environment.NewLine;
+                TBG.Text += "G" + count + "={  ";
+                for (int k = 0; k < G.Length; k++)
+                    if (G[k] != 0)
+                        TBG.Text += "x" + Convert.ToString(G[k]) + "; ";
+                TBG.Text += "}" + Environment.NewLine;
+                count++;
+                int delel;
+                for (int k = 0; k < G.Length; k++)
+                    if (G[k] != 0)
+                    {
+                        delel = k;
+                        for (int p = 0; p < Matrix.Length; p++)
+                            for (int j = 0; j < Matrix[p].Length; j++)
+                                if (p == delel || j == delel)
+                                    Matrix[p][j] = 0;
+                    }
+                Output_Matrix(Matrix, dataGridView2);
+                newstartindex(G, ref i);
+                for (int k = 0; k < Tp.Length; k++)
+                {
+                    Tp[k] = 0;
+                    Tm[k] = 0;
+                    G[k] = 0;
+                }
+            }
+                    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BCalc.Visible = true;
+            inpfromfile = false;
+            MessageBox.Show("Будут использованы условия заданные в варианте №9");
+        }
+
+        //private void BInpfromfile_Click_1(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        //        {
+        //            TBG.Clear();
+        //            TBTminus.Clear();
+        //            TBTplus.Clear();
+        //        }
+        //        else
+        //            throw new FormatException("Файл не выбран");
+        //        string fn = openFileDialog1.FileName;
+        //        StreamReader fin = new StreamReader(fn);
+        //        string str;
+        //        string[] str2;
+        //        for (int i = 0; (str = fin.ReadLine()) != null; i++)
+        //        {
+        //            str2 = str.Split(new char[] { ' ' });
+        //            Matrix[i] = new int[Matrix.Length];
+        //            for (int j = 0; j < Matrix.Length; j++)
+        //            {
+        //                Matrix[i][j] = Convert.ToInt32(str2[j]);
+        //            }
+        //        }
+        //        fin.Close();
+        //    }
+        //    catch (FormatException Ex)
+        //    {
+        //        MessageBox.Show(Ex.Message);
+        //    }
+        //}
+    }
+}
+
